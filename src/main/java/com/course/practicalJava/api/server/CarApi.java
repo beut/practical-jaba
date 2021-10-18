360404894d88218c1c4e307d213a2cc7c34a1c96
Jan <beut@wp.pl>
2021-10-18 17:05:55 +0200

    List of cars as endpoint random-cars

--- src/main/java/com/course/practicalJava/api/server/CarApi.java
+++ src/main/java/com/course/practicalJava/api/server/CarApi.java
@@ -7,11 +7,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 @RestController
@@ -31,5 +36,15 @@ public class CarApi {
     public String echo(@RequestBody Car car) {
         LOG.info("Car is {}", car);
         return car.toString();
+    }
+
+    @GetMapping(value = "/random-cars", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Car> randomCars() {
+        List<Car> result = new ArrayList<>();
+        for (int i = 0; i < ThreadLocalRandom.current().nextInt(1, 10); i++) {
+            result.add(carService.generateCar());
+        }
+        return result;
+
     }
 }
