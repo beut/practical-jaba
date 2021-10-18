3ca0223f423d356dd95887550cf683cdfa732cc3
Jan <beut@wp.pl>
2021-10-18 13:42:26 +0200

    POST with data
    POST method echo added

--- src/main/java/com/course/practicalJava/api/server/CarApi.java
+++ src/main/java/com/course/practicalJava/api/server/CarApi.java
@@ -2,19 +2,23 @@ package com.course.practicalJava.api.server;
 
 import com.course.practicalJava.entity.Car;
 import com.course.practicalJava.service.CarService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 
 @RestController
 @RequestMapping(value = "/api/car/v1")
 public class CarApi {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CarApi.class);
     @Autowired
     private CarService carService;
 
@@ -23,5 +27,9 @@ public class CarApi {
         return carService.generateCar();
     }
 
-
+    @PostMapping(value = "/echo", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String echo(@RequestBody Car car) {
+        LOG.info("Car is {}", car);
+        return car.toString();
+    }
 }
--- src/main/java/com/course/practicalJava/entity/Car.java
+++ src/main/java/com/course/practicalJava/entity/Car.java
@@ -46,5 +46,6 @@ public class Car {
     }
 
 
-
+    public Car() {
+    }
 }
