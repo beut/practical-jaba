58cb2f234c92e7fcb91a930205437c787027a603
Jan <beut@wp.pl>
2021-10-18 15:23:02 +0200

    handling new parameters of car (available, firstReleaseDate, price)

--- src/main/java/com/course/practicalJava/service/RandomCarService.java
+++ src/main/java/com/course/practicalJava/service/RandomCarService.java
@@ -1,8 +1,10 @@
 package com.course.practicalJava.service;
 
 import com.course.practicalJava.entity.Car;
+import com.course.practicalJava.util.RandomDateUtil;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
@@ -15,6 +17,16 @@ public class RandomCarService implements CarService {
         String color = COLORS.get(ThreadLocalRandom.current().nextInt(0, COLORS.size()));
         String type = TYPES.get(ThreadLocalRandom.current().nextInt(0, TYPES.size()));
 
-        return new Car(brand, color, type);
+        Boolean available = ThreadLocalRandom.current().nextBoolean();
+        int price = ThreadLocalRandom.current().nextInt(5000, 12001);
+        LocalDate firstReleaseDate = RandomDateUtil.generateRandomLocalDate();
+
+
+
+        Car result =  new Car(brand, color, type);
+        result.setAvailable(available);
+        result.setFirstReleaseDate(firstReleaseDate);
+        result.setPrice(price);
+        return result;
     }
 }
