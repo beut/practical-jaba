af7fee0d74a63239cce145ff553b6f25fa542425
Jan <beut@wp.pl>
2021-10-18 13:55:36 +0200

    new atributes for car

--- src/main/java/com/course/practicalJava/entity/Car.java
+++ src/main/java/com/course/practicalJava/entity/Car.java
@@ -1,10 +1,15 @@
 package com.course.practicalJava.entity;
 
+import java.time.LocalDate;
+
 public class Car {
 
     private String brand;
     private String color;
     private String type;
+    private int price;
+    private boolean available;
+    private LocalDate firstReleaseDate;
 
     public String getBrand() {
         return brand;
@@ -30,22 +35,58 @@ public class Car {
         this.type = type;
     }
 
-    @Override
-    public String toString() {
-        return "Car{" +
-                "brand='" + brand + '\'' +
-                ", color='" + color + '\'' +
-                ", type='" + type + '\'' +
-                '}';
-    }
-
     public Car(String brand, String color, String type) {
         this.brand = brand;
         this.color = color;
         this.type = type;
     }
 
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                ", type='" + type + '\'' +
+                ", price=" + price +
+                ", available=" + available +
+                ", firstReleaseDate=" + firstReleaseDate +
+                '}';
+    }
 
     public Car() {
+    }
+
+    public Car(String brand, String color, String type, int price, boolean available, LocalDate firstReleaseDate) {
+        this.brand = brand;
+        this.color = color;
+        this.type = type;
+        this.price = price;
+        this.available = available;
+        this.firstReleaseDate = firstReleaseDate;
+    }
+
+    public int getPrice() {
+
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public LocalDate getFirstReleaseDate() {
+        return firstReleaseDate;
+    }
+
+    public void setFirstReleaseDate(LocalDate firstReleaseDate) {
+        this.firstReleaseDate = firstReleaseDate;
     }
 }
