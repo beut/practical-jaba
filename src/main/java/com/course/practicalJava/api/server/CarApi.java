8295875c451af00fae9bfdd325d0aa3d20183e64
Jan <beut@wp.pl>
2021-10-21 00:11:00 +0200

    New find methods for repository

--- src/main/java/com/course/practicalJava/api/server/CarApi.java
+++ src/main/java/com/course/practicalJava/api/server/CarApi.java
@@ -1,19 +1,24 @@
 package com.course.practicalJava.api.server;
 
 import com.course.practicalJava.entity.Car;
+import com.course.practicalJava.repository.CarElasticRepository;
 import com.course.practicalJava.service.CarService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -26,6 +31,9 @@ public class CarApi {
     private static final Logger LOG = LoggerFactory.getLogger(CarApi.class);
     @Autowired
     private CarService carService;
+
+    @Autowired
+    private CarElasticRepository carElasticRepository;
 
     @RequestMapping(value = "/random", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Car random() {
@@ -47,4 +55,62 @@ public class CarApi {
         return result;
 
     }
+
+    @GetMapping(value = "/count")
+    public String countCars() {
+        return "There are: " + carElasticRepository.count() + " cars in ES";
+    }
+
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String saveCar(@RequestBody Car car) {
+        String id = carElasticRepository.save(car).getId();
+
+        return "Saved with id: " +id;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Car getCar(@PathVariable("id") String carId) {
+
+        return carElasticRepository.findById(carId).orElse(null);
+    }
+
+    @PutMapping(value = "/{id}")
+    public String updateCar(@PathVariable("id") String carId, @RequestBody Car updatedCar) {
+
+        updatedCar.setId(carId);
+        String id = carElasticRepository.save(updatedCar).getId();
+        return "updated car with id "+id;
+
+    }
+
+    @GetMapping(value = "/find-json", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Car> findByBrandAndColor(@RequestBody Car car) {
+        return carElasticRepository.findByBrandAndColor(car.getBrand(), car.getColor());
+    }
+
+    @GetMapping(value = "/cars/{brand}/{color}")
+    public List<Car> findCarsByPath(@PathVariable String brand, @PathVariable String color) {
+        return carElasticRepository.findByBrandAndColor(brand, color);
+    }
+
+    @GetMapping(value = "/cars")
+    public List<Car> findCarByParam(@RequestParam String brand, @RequestParam String color) {
+
+        return carElasticRepository.findByBrandAndColor(brand, color);
+
+    }
+
+
+    //dane w params
+    @GetMapping(value = "/cars/date")
+    public List<Car> findCarsReleasedAfter(@RequestParam(name = "first_release_date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate firstReleaseDate) {
+        return carElasticRepository.findByFirstReleaseDateAfter(firstReleaseDate);
+
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/course/practicalJava/repository/CarElasticRepository.java
+++ src/main/java/com/course/practicalJava/repository/CarElasticRepository.java
@@ -4,9 +4,14 @@ import com.course.practicalJava.entity.Car;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 // wrzucamy do bazy elementy typu Car, string jest type of identifier field
 @Repository
 public interface CarElasticRepository extends ElasticsearchRepository<Car, String> {
 
+    public List<Car> findByBrandAndColor(String brand, String color );
 
+    public List<Car> findByFirstReleaseDateAfter(LocalDate date);
 }
