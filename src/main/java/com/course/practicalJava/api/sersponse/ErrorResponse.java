c5dc15f161d457ca46029aa811f41eb410040610
Jan <beut@wp.pl>
2021-10-21 11:44:52 +0200

    Error response handling and headers

---
+++ src/main/java/com/course/practicalJava/api/sersponse/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.course.practicalJava.api.sersponse;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public ErrorResponse(String message, LocalDateTime timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public ErrorResponse() {
+    }
+
+
+}
---
+++ src/main/java/com/course/practicalJava/api/server/DefaultRestApi.java
@@ -0,0 +1,49 @@
+package com.course.practicalJava.api.server;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping(value = "/api")
+
+public class DefaultRestApi {
+
+    private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
+
+    @RequestMapping(value = "/welcome")
+    public String welcome() {
+//        System.out.println(StringUtils.join("Welcome", " Idea", "JS"));
+        LOG.info(StringUtils.join("Welcome", " Idea", "JS"));
+        return "Welcome to Rest api";
+    }
+
+    @RequestMapping("/time")
+    public String time() {
+        return LocalTime.now().toString();
+    }
+
+    @GetMapping(value = "/header-one")
+    public String headerByAnnotation(@RequestHeader(name = "User-agent") String headerUserAgent, @RequestHeader(name="Practical-java", required = false) String headerPracticalJava ) {
+        return "User-agent : " + headerUserAgent + " Practical-java : " + headerPracticalJava;
+    }
+
+    @GetMapping(value = "/header-two")
+    //package reactive!!
+    public String headerByRequest(ServerHttpRequest request) {
+
+        return "User-agent : " + request.getHeaders().getValuesAsList("User-agent") + ", Practical-java : "
+                + request.getHeaders().getValuesAsList("Practical-java");
+
+    }
+
+
+}
+
